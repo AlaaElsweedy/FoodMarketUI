f2c67b4f6bd32b1d76f6ca101969c04522a7f6e5
Alaa Elsweedy <alaaelsweedy12@gmail.com>
2021-02-27 07:19:25 +0200

    finish detail screen

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -28,93 +28,155 @@ class _DetailScreenState extends State<DetailScreen> {
           IconButton(icon: Icon(Icons.shopping_bag_rounded), onPressed: () {}),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Stack(
-          children: [
-            Container(
-              height: size.height * 0.5,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Center(
-                    child: Hero(
-                      tag: widget.food.foodImage,
-                      child: Image.asset(
-                        "assets/images/foods/${widget.food.foodImage}.png",
-                        height: size.height * 0.3,
+      body: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Center(
+                  child: Hero(
+                    tag: widget.food.foodImage,
+                    child: Image.asset(
+                      "assets/images/foods/${widget.food.foodImage}.png",
+                      height: size.height * 0.3,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 30),
+                Text(widget.food.foodName, style: header),
+                SizedBox(height: 20),
+                Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Text(
+                            "\$${widget.food.foodPrice}",
+                            style: TextStyle(color: Colors.white, fontSize: 19),
+                          )
+                        ],
                       ),
-                    ),
+                      Row(
+                        children: [
+                          SizedBox(
+                            height: 30,
+                            width: 30,
+                            child: RaisedButton(
+                              padding: EdgeInsets.zero,
+                              onPressed: () {
+                                setState(() {
+                                  number++;
+                                });
+                              },
+                              color: primaryColor,
+                              child: Icon(Icons.add),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 8),
+                            child: Text(
+                              "$number Kg",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 30,
+                            width: 30,
+                            child: RaisedButton(
+                              padding: EdgeInsets.zero,
+                              onPressed: () {
+                                setState(() {
+                                  if (number <= 1) {
+                                    setState(() {
+                                      number = 1;
+                                    });
+                                  } else {
+                                    setState(() {
+                                      number--;
+                                    });
+                                  }
+                                });
+                              },
+                              color: primaryColor,
+                              child: Icon(Icons.remove),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
-                  SizedBox(height: 30),
-                  Text(widget.food.foodName, style: header),
-                  SizedBox(height: 20),
-                  Container(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Row(
-                          children: [
-                            Text(
-                              "\$${widget.food.foodPrice}",
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 17),
-                            )
-                          ],
+                ),
+                Expanded(
+                  child: ListView(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+                        child: Text(
+                          "Green Cabbage – The king of cabbages and our old friend!"
+                          " The wide fan-like leaves are pale green in color and with a slightly rubbery texture when raw."
+                          " Pick heads that are tight and feel heavy for their size"
+                          " The outer few layers are usually wilted and should be discarded before preparing"
+                          "Green Cabbage – The king of cabbages and our old friend!"
+                          " The wide fan-like leaves are pale green in color and with a slightly rubbery texture when raw."
+                          " Pick heads that are tight and feel heavy for their size."
+                          " The outer few layers are usually wilted and should be discarded before preparing."
+                              "Green Cabbage – The king of cabbages and our old friend!"
+                              " The wide fan-like leaves are pale green in color and with a slightly rubbery texture when raw."
+                              " Pick heads that are tight and feel heavy for their size"
+                              " The outer few layers are usually wilted and should be discarded before preparing"
+                              "Green Cabbage – The king of cabbages and our old friend!"
+                              " The wide fan-like leaves are pale green in color and with a slightly rubbery texture when raw."
+                              " Pick heads that are tight and feel heavy for their size."
+                              " The outer few layers are usually wilted and should be discarded before preparing.",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(color: thirdColor),
                         ),
-                        Row(
-                          children: [
-                            SizedBox(
-                              height: 30,
-                              width: 30,
-                              child: RaisedButton(
-                                padding: EdgeInsets.zero,
-                                onPressed: () {
-                                  setState(() {number ++;});
-                                },
-                                color: primaryColor,
-                                child: Icon(Icons.add),
-                              ),
-                            ),
-                            Padding(
-                              padding:
-                                  const EdgeInsets.symmetric(horizontal: 8),
-                              child: Text(
-                                "$number",
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                            ),
-                            SizedBox(
-                              height: 30,
-                              width: 30,
-                              child: RaisedButton(
-                                padding: EdgeInsets.zero,
-                                onPressed: () {
-                                  setState(() {
-                                    if (number == 0) {
-                                      setState(() {number = 0;});
-                                    } else {
-                                      setState(() {number--;});
-                                    }
-                                  });
-                                },
-                                color: primaryColor,
-                                child: Icon(Icons.remove),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
+                      ),
+                      SizedBox(height: 50,),
+                    ],
                   ),
-                ],
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+              height: size.height * 0.1,
+              width: double.infinity,
+              child: RaisedButton(
+                color: primaryColor,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)),
+                onPressed: () {},
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.shopping_cart,
+                      color: Colors.white,
+                      size: 35,
+                    ),
+                    SizedBox(width: 10),
+                    Text(
+                      "Add TO Cart",
+                      style: TextStyle(color: Colors.white, fontSize: 25),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
